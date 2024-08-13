e48cfeea9d060a1917b027b6a7e30b798203df0c
lauraais <laura@aishore.ai>
2024-08-13 10:51:25 -0400

    reviewed hugging face models code and added requirements

--- models.py
+++ models.py
@@ -11,22 +11,41 @@ print(type(data))
 ####### END #######
 
 ####### xgen 7b 8k #######
-# tokenizer = AutoTokenizer.from_pretrained("Salesforce/xgen-7b-8k-base", trust_remote_code=True)
-# model = AutoModelForCausalLM.from_pretrained("Salesforce/xgen-7b-8k-base", torch_dtype=torch.bfloat16)
-# inputs = tokenizer("The world is", return_tensors="pt")
-# sample = model.generate(**inputs, max_length=128)
-# print(tokenizer.decode(sample[0]))
+tokenizer = AutoTokenizer.from_pretrained("Salesforce/xgen-7b-8k-base", trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained("Salesforce/xgen-7b-8k-base", torch_dtype=torch.bfloat16)
+inputs = tokenizer("The world is", return_tensors="pt")
+sample = model.generate(**inputs, max_length=128)
+print(tokenizer.decode(sample[0]))
 
 # trying to run the above xgen, but when I run, it gets stuck after saying 
 # "Setting `pad_token_id` to `eos_token_id`:50265 for open-end generation"
+# ended up with this response: 
+# Setting `pad_token_id` to `eos_token_id`:50256 for open-end generation.
+# The world is full of people who are not happy with their lives. They are not happy with their jobs, their relationships, their families, their friends, their health, their finances, their appearance, their pasts, their futures, their lives. They are not happy with anything.
+
+# They are not happy with themselves.
+
+# They are not happy with the world.
+# They are not happy with the universe.
+
+# They are not happy with life.
+
+# They are not happy with death.
+
+# They are not happy with the past.
+
+# They are not happy with the future.
+
+# They are
+
 
 ####### qwen 1.5 110b 32k #######
-qwen_model = "Qwen/Qwen1.5-110B"
-messages = [
-    {"role": "user", "content": "Who are you?"},
-]
-pipe = pipeline("text-generation", model="Qwen/Qwen1.5-110B")
-pipe(messages) 
+# qwen_model = "Qwen/Qwen1.5-110B"
+# messages = [
+#     {"role": "user", "content": "Who are you?"},
+# ]
+# pipe = pipeline("text-generation", model="Qwen/Qwen1.5-110B")
+# pipe(messages) 
 
 
 # ####### Mixtral 8x 22B-Instruct 64k #######
